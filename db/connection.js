c25d834a39249f02bcdae6c7cf8951d6649ad583
yang <joey674901205@gmail.com>
2022-07-27 18:16:32 -0400

    add npm dotenv to hide mysql password, create .env file

--- db/connection.js
+++ db/connection.js
@@ -1,19 +1,12 @@
 const mysql = require('mysql2');
 
-// const db = mysql.createConnection(
-//     {
-//         host: 'localhost',
-//         user: 'root',
-//         password: 'yangbao94Wly',
-//         database: 'employee_tracker'
-//     }
-// );
+require('dotenv').config();
 
 const pool = mysql.createPool(
     {
         host: 'localhost',
         user: 'root',
-        password: 'yangbao94Wly',
+        password: process.env.DB_PASSWORD,
         database: 'employee_tracker'
     }
 );
